8ca6c4d5ef9a9d1c8f3f8478e8d05ce3daa3fd9a
Paul Kimball <pekimball@verizon.net>
2023-06-21 11:41:49 -0400

    Main program that will work when you are done

--- src/main/java/org/yearup/Main.java
+++ src/main/java/org/yearup/Main.java
@@ -6,5 +6,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("/beans.xml");
+        ProductDao dao = ctx.getBean(ProductDao.class);
+
+        Product p1 = new Product("Beans", "Food", 2.00);
+        Product p2 = new Product("Tuna", "Food", 3.25);
+
+        dao.add(p1);
+        dao.add(p2);
+
+        Product p = dao.findByProductId(101L);
+
+        System.out.println(p);
+
     }
 }
